093e0b20ad590c4ea86a2552f383c058ff34d66e
LamRAM <150258612+LamRAM@users.noreply.github.com>
2024-06-24 11:25:11 +0300

    Create 01.introduction.py

---
+++ labs/01.introduction.py
@@ -0,0 +1,26 @@
+# 1.1 Identify and fix the errors in this example.
+# 20 is the person age
+if 20 < 18 
+    print("you are an adult")
+print("End")
+
+# 1.2 Comment out line number 3 in this example
+if 20 >= 18:
+    print("you are an adult")
+print("End")
+
+# 2.1 Identify and fix the error
+if 20 > 10;
+    print("20 is greater than 10")
+
+# 3.1 What is the output of the following code
+print("begin")
+for i in [1,2]:
+    print("hi")
+    print("end")
+
+# 4.1 What is the output of the following code
+print("begin")
+for i in [1,2]:
+    print("hi")
+print("end")
